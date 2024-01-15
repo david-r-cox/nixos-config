90bd854d6521306970cbd72a4a4085bd64903be2
David Cox <david@integrated-reasoning.com>
2024-01-14 18:02:52 -1000

    style(home.nix): reduce redundancy using inherit

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,12 +1,18 @@
 { pkgs, lib, ... }:
+let
+  username = "david";
+  inherit (pkgs.stdenv) isLinux;
+  inherit (pkgs.stdenv) isDarwin;
+  inherit (lib) optionalAttrs;
+in
 rec {
   home.stateVersion = "23.05";
 
   home = {
-    username = "david";
+    inherit username;
     homeDirectory =
-      if pkgs.stdenv.isDarwin then "/Users/${home.username}"
-      else "/home/${home.username}";
+      if isDarwin then "/Users/${username}"
+      else "/home/${username}";
     file = import ./dotfiles;
     sessionVariables = import ./session;
     packages = import ./packages { inherit pkgs; };
@@ -18,35 +24,38 @@ rec {
     bash = import programs/bash;
     broot = import programs/broot;
     fzf = import programs/fzf;
-    git = import programs/git
-      {
-        isCorp = false;
-      };
+    git = import programs/git { isCorp = false; };
     home-manager.enable = true;
     neovim = import programs/neovim { inherit pkgs; };
     nix-index = import programs/nix-index;
     zsh = import programs/zsh { inherit pkgs; };
-  } // lib.optionalAttrs pkgs.stdenv.isLinux {
+  }
+  // optionalAttrs isLinux {
     alacritty = import programs/alacritty {
       colorscheme = import ./colorschemes/ayu_dark.nix;
       inherit pkgs;
     };
-    rofi = import ./programs/rofi { inherit pkgs; };
+    rofi = import ./programs/rofi {
+      inherit pkgs;
+    };
   };
 
-  systemd = lib.optionalAttrs pkgs.stdenv.isLinux {
+  systemd = optionalAttrs isLinux {
     user.services = import systemd/user/services {
       inherit (home) homeDirectory;
       inherit pkgs;
     };
   };
-  services = lib.optionalAttrs pkgs.stdenv.isLinux {
+  services = optionalAttrs isLinux {
     polybar = {
       enable = true;
       script = "polybar -c ~/.config/polybar/config.ini &";
     };
   };
-  xdg = lib.optionalAttrs pkgs.stdenv.isLinux {
+  xdg = optionalAttrs isLinux {
     configFile = import ./xdg/config;
+  };
+  xsession = optionalAttrs isLinux {
+    windowManager.xmonad = import ./programs/xmonad { };
   };
 }
