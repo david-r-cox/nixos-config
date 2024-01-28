5e665aa9bd3bfd71174766f8a1edf04542cb8d6e
David Cox <david@integrated-reasoning.com>
2024-01-27 22:37:43 -1000

    style(starship): update prompt

--- home-manager/programs/starship/default.nix
+++ home-manager/programs/starship/default.nix
@@ -6,5 +6,36 @@
       success_symbol = "[➜](bold green)";
       error_symbol = "[➜](bold red)";
     };
+    line_break.disabled = true;
+    format = "$os$all";
+    os = {
+      format = "[($name $symbol $version)]($style) ";
+      style = "bold blue";
+      disabled = true;
+      symbols = {
+        Alpine = "🏔️";
+        Amazon = "🙂";
+        Arch = "🎗️";
+        Debian = "🌀";
+        FreeBSD = "😈";
+        Gentoo = "🗜️";
+        Linux = "🐧";
+        Macos = "🍎";
+        NixOS = "❄️";
+        OpenBSD = "🐡";
+        Raspbian = "🍓";
+        Unknown = "❓";
+      };
+    };
+    shell = {
+      nu_indicator = "ν";
+      zsh_indicator = "ζ";
+      bash_indicator = "β";
+      style = "bold green";
+      disabled = false;
+    };
+    sudo = {
+      disabled = false;
+    };
   };
 }
