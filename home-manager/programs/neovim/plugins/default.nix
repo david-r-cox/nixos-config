61d518580adc6293976ee18ec76f086e505ff977
David Cox <david@integrated-reasoning.com>
2024-06-06 12:41:49 -0700

    feat(neovim): add ollama-nvim

--- home-manager/programs/neovim/plugins/default.nix
+++ home-manager/programs/neovim/plugins/default.nix
@@ -147,6 +147,32 @@ with vimPlugins; [
       require('litee.calltree').setup({})
     '';
   }
+  {
+    plugin = ollama-nvim;
+    type = "lua";
+    config = ''
+      require('ollama').setup({
+        model = "llama3",
+        url = "http://127.0.0.1:11434",
+        serve = {
+          on_start = false,
+          command = "ollama",
+          args = { "serve" },
+          stop_command = "pkill",
+          stop_args = { "-SIGTERM", "ollama" },
+        },
+        prompts = {
+          Sample_Prompt = {
+            prompt = "This is a sample prompt that receives $input and $sel(ection), among others.",
+            input_label = "> ",
+            model = "llama3",
+            action = "display",
+          }
+        }
+      })
+      vim.keymap.set('x', '<leader>i', ':Ollama<cr>')
+    '';
+  }
 ] ++ [
   #coc-clangd
   coc-nvim
