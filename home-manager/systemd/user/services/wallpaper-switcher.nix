f7d76b20d7e706f51c6cc72902a50ec0ee5633b7
David Cox <david@integrated-reasoning.com>
2025-01-10 12:04:31 -0800

    feat: add static mode to wallpaper-switcher service

--- home-manager/systemd/user/services/wallpaper-switcher.nix
+++ home-manager/systemd/user/services/wallpaper-switcher.nix
@@ -11,17 +11,37 @@
       #!/usr/bin/env bash
       WP_DIR='${homeDirectory}/documents/wallpaper/widescreen'
       PATH=/run/current-system/sw/bin:$PATH
-      while true
-      do
+
+      # Toggle between modes (0: random switcher, 1: static)
+      MODE=1
+
+      # Pre-defined static wallpaper paths
+      STATIC_CENTER="$WP_DIR/6zlp1jd1ar691.png"
+      STATIC_LEFT="$WP_DIR/6zlp1jd1ar691.png"
+      STATIC_RIGHT="$WP_DIR/6zlp1jd1ar691.png"
+
+      # Sleep duration for each mode (in seconds)
+      RANDOM_SLEEP=60
+
+      if [ $MODE -eq 0 ]; then
+        # Random switcher mode
+        while true
+        do
           filesleft=($(find "$WP_DIR" -type f | shuf))
           for fileleft in "$filesleft"; do
               fileright=($(find "$WP_DIR" -type f | shuf | head -n 1))
               filecenter=($(find "$WP_DIR" -type f | shuf | head -n 1))
               echo $fileleft $filecenter $fileright
               ${pkgs.feh}/bin/feh --bg-fill "$fileleft" "$filecenter" "$fileright"
-              sleep 60.0
+              sleep $RANDOM_SLEEP
           done
-      done
+        done
+      else
+        # Static wallpaper mode
+        echo $STATIC_LEFT $STATIC_CENTER $STATIC_RIGHT
+        ${pkgs.feh}/bin/feh --bg-fill "$STATIC_LEFT" "$STATIC_CENTER" "$STATIC_RIGHT"
+        # No need for a loop in static mode, it sets once and exits
+      fi
     '');
   };
 }
