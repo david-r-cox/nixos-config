2cdd1d058255fd971904ffe47cf202c7de492062
David Cox <david@integrated-reasoning.com>
2025-02-22 23:05:10 -0800

    fix: add x86_64-darwin to platformPackages

--- flake.nix
+++ flake.nix
@@ -57,8 +57,8 @@
         napali
       ];
       platformPackages = {
-        "x86_64-linux" = [
-        ];
+        "x86_64-linux" = [ ];
+        "x86_64-darwin" = [ ];
         "aarch64-linux" = [ ];
         "aarch64-darwin" = [ ];
       };
