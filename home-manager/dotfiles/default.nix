66fe938ac1933241b40d850cb70ad6a2847e52e6
David Cox <david@integrated-reasoning.com>
2025-09-23 16:10:35 -0700

    feat(dotfiles): install iterm2 preferences via home-manager

--- home-manager/dotfiles/default.nix
+++ home-manager/dotfiles/default.nix
@@ -1,5 +1,6 @@
-{pkgs}:
+{ pkgs }:
 {
+  # TODO: This should only apply to linux
   ".config/polybar/config.ini".text = builtins.readFile (./polybar/config.ini);
   # wd required a mutable warprc file:
   #".warprc".text = builtins.readFile (./wd/warprc);
@@ -8,4 +9,7 @@
     source = "${pkgs.nerd-fonts.fira-code}/share/fonts/truetype/NerdFonts/FiraCode";
     recursive = true;
   };
+  # TODO: This should only apply to macOS
+  "Library/Application Support/iTerm2/com.googlecode.iterm2.plist".text =
+    builtins.readFile (./iterm2/iterm2.plist);
 }
