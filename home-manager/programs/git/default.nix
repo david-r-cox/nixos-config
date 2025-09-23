e4b76786ce6c77be9c61bda950638aee84863138
David Cox <david@integrated-reasoning.com>
2025-09-23 16:12:16 -0700

    feat(git): enable git-lfs integration

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -2,13 +2,8 @@
 {
   enable = true;
   userName = "David Cox";
-  userEmail =
-    if isCorp
-    then "david@integrated-reasoning.com"
-    else "david_cox@hey.com";
-  signing.key =
-    if isCorp
-    then "89FA74890EFDDC3B"
-    else "918102E888149978";
+  userEmail = if isCorp then "david@integrated-reasoning.com" else "david_cox@hey.com";
+  signing.key = if isCorp then "89FA74890EFDDC3B" else "918102E888149978";
   signing.signByDefault = true;
+  lfs.enable = true;
 }
